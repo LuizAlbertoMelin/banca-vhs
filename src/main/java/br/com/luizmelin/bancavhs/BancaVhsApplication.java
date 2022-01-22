5a3cfa09c6b99009b1074d26cc9b2939b4213b00
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-01-22 19:26:08 -0300

    @LuizAlbertoMeliÂn[Ãcriando banner]

---
+++ src/main/java/br/com/luizmelin/bancavhs/BancaVhsApplication.java
@@ -0,0 +1,17 @@
+package br.com.luizmelin.bancavhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaVhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancaVhsApplication.class, args);
+
+		System.out.println(
+			"\nProjeto Banca VHS iniciado com sucesso"
+		);
+	}
+
+}
---
+++ src/test/java/br/com/luizmelin/bancavhs/BancaVhsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizmelin.bancavhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaVhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
